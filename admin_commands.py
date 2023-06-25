7bb13d169808115e13745b5aaca95c840a9be181
VysePresident <imgenius77x7@gmail.com>
2023-06-25 02:44:29 -0500

    Fixes and Streamlining
    1) The test key is now stored in a separate file and a try/catch statement has been implemented to first check for actual bot key, then test bot key, then finally to note failure.
    
    2) Removed unnecessary comments and obsolete code.
    
    3) Added 40 dashes to initial votecount message.
    
    4) Created working config file & import statement.

--- admin_commands.py
+++ admin_commands.py
@@ -5,11 +5,15 @@ import config
 class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.signup_list = []
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def startsignup(self, ctx):
-        # Your code here ...
+        global signups_open
+        signups_open = True
+        signup_list.clear()
+        await ctx.send('Sign ups are open! Sign up for the game by doing %signup')
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 from datetime import datetime
 import collections
 
-signups_open = False
-signup_list = []
+# Game Setup
+signups_open = False # Pregame
 vote_channel = None
 game_channel = None
-day_number = 0
-vote_count_number = 1
-votes = {} 
-day_end_time = None
 global_day_length = 1
+day_end_time = 1
+
+# Game Status
+day_number = 0
+signup_list = [] # Pre & Mid
+vote_count_number = 1
+# votes = {}
+votes = collections.OrderedDict()
+prev_vote = None
+current_vote = None
 live_players = []
 vote_since_last_count = 0
 start_time = datetime.now()
+
 abstained_players = []
+
+# Post count collection
 post_counts = collections.defaultdict(lambda: collections.defaultdict(int))
