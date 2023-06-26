735c3103554beb4526048eb913bd24738236195b
VysePresident <imgenius77x7@gmail.com>
2023-06-26 05:05:03 -0500

    Massive Cog Rework
    Split the role_repository into it's own file.
    
    Implemented AdminCommands in the admin_commands.py file.  Cog added to bot in the on_ready() function.
    
    Created admin_commands function "modkill" to ensure that only the mod can kill, without permission conflicts occurring due to "kill" callback in votecount().
    
    Split commands from bot.py into cogs "PlayerCommands" in player_commands.py and "AdminCommands" in admin_commands.py.
    
    Fixed a bug where the %changedaylength command would not change the time in the current Day phase.
    
    PROBLEM: kill() is still a command.  This should not be accessible to any non-administrator.  Need to go back and fix it.
    
    Cleaned up a ton of excess code.
    
    Temporarily commented out Role Related Commands.

--- config.py
+++ config.py
@@ -1,26 +1,30 @@
+import asyncio
 from datetime import datetime
 import collections
 
-# Game Setup
-signups_open = False # Pregame
-vote_channel = None
-game_channel = None
-global_day_length = 1
-day_end_time = 1
+class Config():
+    # Game Setup
+    signups_open = False  # Pregame
+    vote_channel = None
+    game_channel = None
+    global_day_length = 1
+    day_end_time = 1
 
-# Game Status
-day_number = 0
-signup_list = [] # Pre & Mid
-vote_count_number = 1
-# votes = {}
-votes = collections.OrderedDict()
-prev_vote = None
-current_vote = None
-live_players = []
-vote_since_last_count = 0
-start_time = datetime.now()
+    day_end_task_object = None
 
-abstained_players = []
+    # Game Status
+    day_number = 0
+    signup_list = []  # Pre & Mid
+    vote_count_number = 1
+    # votes = {}
+    votes = collections.OrderedDict()
+    prev_vote = None
+    current_vote = None  # Unused atm
+    live_players = []
+    vote_since_last_count = 0
+    start_time = datetime.now()  # Unused atm
 
-# Post count collection
-post_counts = collections.defaultdict(lambda: collections.defaultdict(int))
+    abstained_players = []
+
+    # Post count collection
+    post_counts = collections.defaultdict(lambda: collections.defaultdict(int))
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from player_commands import perform_player_action
+
+
+
+"""from player_commands import perform_player_action
 from admin_commands import perform_admin_action
 from utility_commands import get_time, get_date
 from settings import initialize_settings
@@ -13,4 +16,4 @@ bot.load_extension("helper_functions")
 bot.load_extension("settings")
 
 if __name__ == "__main__":
-    bot.run(MTExMDA5NjEwNDEwMTUzMTc0OA.GJLsEp.jLmTauJrCUZQ8uH4LJkEs0tDSLVLXC9Q9nb7mE)
+    bot.run(MTExMDA5NjEwNDEwMTUzMTc0OA.GJLsEp.jLmTauJrCUZQ8uH4LJkEs0tDSLVLXC9Q9nb7mE)"""
