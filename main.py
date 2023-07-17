729e85c6048f771038caf72b25f320459d15f15e
VysePresident <imgenius77x7@gmail.com>
2023-07-16 21:24:04 -0500

    Cleanup
    Removed obsolete code.
    
    Adding print statements for debugging (will replace with actual logging, but see no reason to remove right now.)
    
    Fixed swapplayer command.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import discord
-from discord.ext import commands
 from bot import MafiaBot
-from admin_commands import AdminCommands
-from player_commands import PlayerCommands
 
 bot_token = ''
 try:
@@ -20,13 +16,8 @@ except ImportError:
 
 bot = MafiaBot(command_prefix="%", token=bot_token)
 
-admin_commands = AdminCommands(bot)
-player_commands = PlayerCommands(bot)
-
 @bot.event
 async def on_ready():
-    # await bot.add_cog(admin_commands)
-    # await bot.add_cog(player_commands)
 
     await bot.load_extension("admin_commands")
     await bot.load_extension("player_commands")
@@ -34,210 +25,11 @@ async def on_ready():
     if bot.get_channel(1110103139065012244):
         channel = bot.get_channel(1110103139065012244)
         await channel.send('Bot is online!')
-    else:
-        print("Mustard")
+    print("Bot is online")
 
 @bot.command()
 async def reloadext(ctx):
     await bot.reload_extension("admin_commands")
     await bot.reload_extension("player_commands")
 
-
 bot.run(bot_token)
-
-"""
-import discord
-from discord.ext import commands
-import time as t
-import asyncio
-import collections
-
-bot_token = ''
-try:
-    from aso_key_file_name import mysterious_key_of_power
-    print("Main key imported")
-    bot_token = mysterious_key_of_power
-except ImportError:
-    try:
-        from test_token import test_token
-        bot_token = test_token
-        print("Test bot key imported")
-    except ImportError:
-        bot_token = ''
-        print("All imports failed.  Using blank key")
-
-bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
-
-# Import Config and Cogs. These are added in the on_ready() event listener.
-
-from config import Config
-
-from admin_commands import AdminCommands
-admin_commands = AdminCommands(bot)
-
-from player_commands import PlayerCommands
-player_commands = PlayerCommands(bot)
-
-@bot.event
-async def on_ready():
-    await bot.add_cog(admin_commands)
-    await bot.add_cog(player_commands)
-
-    if bot.get_channel(1110103139065012244):
-        channel = bot.get_channel(1110103139065012244)
-        await channel.send('Bot is online!')
-    else:
-        print("Mustard")
-
-@bot.command()
-async def votecount(ctx, in_channel_request=True, vote_change=None, is_unvote=False):
-    if in_channel_request is not True and in_channel_request is not False:
-        await ctx.send("You specified the wrong number of parameters, foolish mortal!  Try again or taste my wrath!")
-        return
-    count = collections.OrderedDict()
-    votes_required = len(Config.signup_list)//2 + 1
-
-    # Construct vote count for each player voted.  Should automatically be in order.
-    for voter, voted in Config.votes.items():
-        if voted in count:
-            count[voted].append(voter)
-        else:
-            count[voted] = []
-            count[voted].append(voter)
-
-    # Construct message string
-    game_thread_message = ''
-    votecount_message = f"**Vote Count {Config.day_number}.{Config.vote_count_number} - **{ctx.message.jump_url}\n\n"
-    endDay = False
-    for voted, voters in count.items():
-        remaining_votes = votes_required - len(voters)  # The number of votes remaining for a lynch
-        lynch_status = '**LYNCH**' if remaining_votes == 0 else f"L-{remaining_votes}"
-        voters_str = ', '.join([voter.name if voter is not Config.prev_vote[0] else f'**{voter.name}**' for voter in voters])
-        votecount_message += f"{voted.name}[{lynch_status}] - {voters_str}\n"
-        if lynch_status == '**LYNCH**':
-            endDay = True
-        # Game thread lynch status message
-        if voted is vote_change:
-            game_thread_message = f"{voted.name} is {lynch_status}"
-            # await Config.game_channel.send(f"{voted.name} is {lynch_status}")
-    not_voting = [player for player in Config.signup_list if player not in Config.votes.keys()]
-    if len(not_voting) > 0:
-        votecount_message += f"\nNot Voting - {', '.join([player.name if player is not Config.prev_vote[0] else f'**{player.name}**' for player in not_voting])}"
-    else:
-        votecount_message += f"\nNot Voting - (None)"
-    if isinstance(Config.prev_vote[1], str):
-        votecount_message += f"\n\n__Change__: *{Config.prev_vote[0].name} switched from {Config.prev_vote[1]} to {voted.name}*"
-    elif is_unvote:
-        votecount_message += f"\n\n__Change__: *{Config.prev_vote[0].name} has unvoted {Config.prev_vote[1]}*"
-    else:
-        votecount_message += f"\n\n__Change__: *{Config.prev_vote[0].name} switched from {Config.prev_vote[1].name} to {voted.name}*"
-    votecount_message += f"\n\n*With {len(Config.signup_list)} alive, it takes {votes_required} to lynch.*"
-    votecount_message += "\n" + "-" * 40
-
-    # End of Day message
-    if endDay:
-        # await bot.get_command("kill").callback(ctx, voted)
-        await kill(ctx, voted)
-        votecount_message += f"\n\n**{voted.name} has been removed from the game**"
-        votecount_message += f"\n\n**Night {Config.day_number+1} begins now!**"
-        alive_role = discord.utils.get(ctx.guild.roles, name="Alive")
-        await Config.game_channel.set_permissions(alive_role, send_messages=False)
-        await Config.game_channel.send("The day has ended with a lynch.")
-    if in_channel_request:
-        await ctx.send(votecount_message)
-    else:
-        votecount_sent = await Config.vote_channel.send(votecount_message)
-    # Another in-thread lynch status alternative message
-    if vote_change is not None and vote_change not in count:
-        game_thread_message = f"{vote_change.name} has zero votes"
-        # await Config.game_channel.send(f"{vote_change.name} has zero votes")
-    # Test lynch status message
-    if votecount_sent:
-        game_thread_message += f": {votecount_sent.jump_url}"
-    await Config.game_channel.send(game_thread_message)
-
-    if in_channel_request:
-        Config.vote_count_number += 1
-    return
-
-
-# @commands.command()
-# @commands.has_permissions(Administrator=True)
-async def kill(ctx, member: discord.Member):
-    if member.name in Config.live_players:
-        Config.live_players.remove(member.name)
-        Config.signup_list.remove(member)
-        alive_role = discord.utils.get(ctx.guild.roles, name="Alive")
-        dead_role = discord.utils.get(ctx.guild.roles, name="Dead")
-        Config.day_end_task_object.cancel()  # END DAY PHASE
-        if dead_role is None:
-            dead_role = await ctx.guild.create_role(name="Dead")
-        if alive_role in member.roles:
-            await member.remove_roles(alive_role)
-        if dead_role not in member.roles:
-            await member.add_roles(dead_role)
-        await ctx.send(f"{member.name} has been removed from the game.")
-    else:
-        await ctx.send(f"{member.name} is not in the game or already removed.")
-"""
-
-"""=============================================="""
-
-# ROLE RELATED COMMAND
-"""@bot.command()
-async def roleinfo(ctx, *, role: str):
-    role = role.lower()
-    if role not in roles:
-        await ctx.send('That role cannot be found. Try googling "(rolename) mafiascum wiki".')
-    else:
-        role_info = roles[role]
-        await ctx.send(f'**{role.capitalize()}**\n{role_info["description"]}\n\n**Alignment:** {role_info["alignment"]}\n\n**Special Abilities:** {role_info["special_abilities"]}')
-"""
-
-# ROLE RELATED COMMAND
-"""
-@bot.command()
-async def rolelist(ctx):
-    formatted_roles = [role.capitalize() for role in roles]
-    await ctx.send("**Displaying normal roles:**\n" + '\n'.join(formatted_roles))
-"""
-
-# Command is not used yet.  Review later.
-"""
-async def periodic_votecount_check():
-    while True:
-        if Config.vote_since_last_count >= 10:
-            # Reset the counter
-            Config.vote_since_last_count = 0
-            # Call the votecount method (assumes it takes no arguments, modify as needed)
-            await votecount()
-        # Sleep for 10 seconds before checking again
-        await asyncio.sleep(10)
-"""
-"""=============================================="""
-#bot.loop.create_task(periodic_votecount_check())
-
-# TEST BOT KEY!
-# bot.run(bot_token)
-
-"""=============================================="""
-"""=============================================="""
-"""=============================================="""
-
-"""from player_commands import perform_player_action
-from admin_commands import perform_admin_action
-from utility_commands import get_time, get_date
-from settings import initialize_settings
-
-bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
-bot.remove_command('help')
-
-bot.load_extension("player_commands")
-bot.load_extension("admin_commands")
-bot.load_extension("utility_commands")
-bot.load_extension("helper_functions")
-bot.load_extension("settings")
-
-if __name__ == "__main__":
-    bot.run(MTExMDA5NjEwNDEwMTUzMTc0OA.GJLsEp.jLmTauJrCUZQ8uH4LJkEs0tDSLVLXC9Q9nb7mE)"""
-
--- player_commands.py
+++ player_commands.py
@@ -10,7 +10,9 @@ class PlayerCommands(commands.Cog):
 
     @commands.command()
     async def signup(self, ctx):
-        print("This is signups_open in signup(): " + str(Config.signups_open))
+        # DEBUG LOG
+        print(f"Command signup: Author: {ctx.author.name}")
+
         if Config.signups_open:
             if ctx.author not in Config.signup_list:
                 Config.signup_list.append(ctx.author)
@@ -22,6 +24,9 @@ class PlayerCommands(commands.Cog):
 
     @commands.command()
     async def unsignup(self, ctx):
+        # DEBUG LOG
+        print(f"Command unsignup: Author: {ctx.author.name}")
+
         if ctx.author in Config.signup_list:
             Config.signup_list.remove(ctx.author)
             await ctx.send(f'You have been removed from the sign up list, {ctx.author.name}!')
@@ -30,16 +35,19 @@ class PlayerCommands(commands.Cog):
 
     @commands.command()
     async def signuplist(self, ctx):
-        print("This is some bizarrely violent mustard in signuplist")
+        # DEBUG LOG
+        print(f"Command signuplist: Author: {ctx.author.name}")
         if not Config.signup_list:
             await ctx.send('No one has signed up yet.')
         else:
             signups = "\n".join([user.name for user in Config.signup_list])
-            await ctx.send(f'There are currently {len(Config.signup_list)} signups :\n\n{signups}')
-            # await ctx.send(f'The current signups are:\n{signups}')
+            await ctx.send(f'There are currently {len(Config.signup_list)} signups:\n\n{signups}')
 
     @commands.command()
     async def vote(self, ctx, voted: discord.Member):
+        # DEBUG LOG
+        print(f'command vote: ctx.author: {ctx.author.name} and voted: {voted.name}')
+
         if ctx.author not in Config.signup_list or voted not in Config.signup_list:
             await ctx.send("Either the voter or the voted player is not in the game.")
             return
@@ -58,13 +66,27 @@ class PlayerCommands(commands.Cog):
             Config.prev_vote = (ctx.author, "not voting")
         Config.votes[ctx.author] = voted
 
+        # DEBUG LOGS
+        """print(f'Finishing command vote: ctx.author: {ctx.author.name} and voted: {Config.votes[ctx.author].name}')
+        if ctx.author in Config.votes:
+            # DEBUG
+            print(f'RESULT: This is prev_vote[0]: {Config.prev_vote[0].name} and prev_vote[1]: {Config.prev_vote[1].name}')
+        else:
+            # DEBUG
+            print(f'RESULT: This is prev_vote[0]: {Config.prev_vote[0].name} and prev_vote[1]: {Config.prev_vote[1]}')"""
+
+        # DEBUG LOGS:
+        print(f'Finishing command vote: ctx.author: {ctx.author.name} and voted: {Config.votes[ctx.author].name}')
+        print(f'Finishing vote - prev_vote: prev_vote[0]: {Config.prev_vote[0]} and prev_vote[1]: {Config.prev_vote[1]}')
         await ctx.send(f"{ctx.author.name} has voted for {Config.votes[ctx.author].name}.")  # TEST
-        # await self.bot.get_command("votecount").callback(ctx=ctx, in_channel_request=False, vote_change=voted)
         await self.bot.votecount(self.bot, ctx=ctx, in_channel_request=False, vote_change=voted)
         return
 
     @commands.command()
     async def unvote(self, ctx):
+        # DEBUG LOG
+        print(f'command unvote: ctx.author: {ctx.author.name} has unvoted')
+
         if ctx.channel != Config.game_channel:
             await ctx.send("You can only vote in the game chat.")
             return
@@ -73,28 +95,38 @@ class PlayerCommands(commands.Cog):
             await ctx.send(f"{ctx.author.name} has unvoted.")
             await self.bot.votecount(self.bot, ctx=ctx, in_channel_request=False,
                                                              vote_change=Config.prev_vote[1], is_unvote=True)
-            # await self.bot.get_command("votecount").callback(ctx=ctx, in_channel_request=False,
-            #                                            vote_change=Config.prev_vote[1], is_unvote=True)
         else:
             await ctx.send("You haven't voted yet.")
         return
 
     @commands.command()
+
     async def time(self, ctx):
+        # DEBUG
+        print(f'command time: ctx.author: {ctx.author.name} used %time')
+
         if Config.day_end_time is None:
             await ctx.send("The game hasn't started yet.")
         else:
             time_remaining = Config.day_end_time - t.time()
             if time_remaining <= 0:
+                print(f'RESULT: The day has ended.')
                 await ctx.send("The day has ended.")
             else:
                 hours, rem = divmod(time_remaining, 3600)
                 minutes, seconds = divmod(rem, 60)
+                # DEBUG LOG
+                print(f'RESULT of {ctx.author.name} using %time: '
+                      f'Time remaining: {int(hours)} hours, {int(minutes)} minutes, and {int(seconds)} seconds.')
+
                 await ctx.send(
                     f"Time remaining: {int(hours)} hours, {int(minutes)} minutes, and {int(seconds)} seconds.")
 
     @commands.command()
     async def playerlist(self, ctx):
+        # DEBUG
+        print(f'command time: ctx.author: {ctx.author.name} used %playerlist')
+
         alive_players = []
         dead_players = []
         for member in ctx.guild.members:
@@ -106,7 +138,11 @@ class PlayerCommands(commands.Cog):
             alive_players.append("No players alive.")
         if not dead_players:
             dead_players.append("No players dead.")
-        await ctx.send(f"Alive players:\n{' '.join(alive_players)}\n\nDead players:\n{' '.join(dead_players)}")
+        playerlist_string = "\n\n__**Alive Players**__\n" + '\n'.join(alive_players) + '\n'
+        playerlist_string += "\n__**Dead Players**__\n" + '\n'.join(dead_players)
+        print(f"This is result of {ctx.author.name} using playerlist: {playerlist_string}")
+        await ctx.send(f"{playerlist_string}")
+
 
 async def setup(bot):
     await bot.add_cog(PlayerCommands(bot))
