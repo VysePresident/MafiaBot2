541299a24ba370a78ef5be691a33b7e227dbc018
VysePresident <imgenius77x7@gmail.com>
2023-07-31 05:37:32 -0500

    Massive Player Object Rework and Database Skeleton Update
    Reworked all commands to use the Player class and signup_list is now a dictionary in the form {member: player} where member is a Discord Member object and player is a Player object.
    
    Added a reset method to the Config class that resets all the static values.  Added a bunch of other minor methods and attributes to make interacting with Config values simpler.
    
    Added "db.py" which contains the DatabaseManger class.  This class will be used to as a go-between for various other functions to interact with the database.
    
    Fixed possible bug involving %newday and votecounts, in which Config.votes was set to a regular dictionary and would likely have caused votes to appear out of order after D1.
    
    I've added a small adjustment to %signuplist so that it displays a number next to the name of the person signing up.
    
    Italicized the changed votes in the "CHANGE: " section of the votecount.
    
    Added the configReset(), votesReset(), and the newDay() methods to Config.  The purpose is to standardize how these things are done.
    
    Added the dev command configReport() to see the values currently in Config at any given time.
    Added module/function information comments.
    
    Endgame message is now pinned.
    
    The program now uses the Player.kill() function instead of a kill command.
    
    The votecount function now reports (None) if there are no votes.
    
    The playerlist command now reports (None) if there are no Alive players or Dead players.
    
    Votecount() function now cancels the Day End task.
    
    So many minor fixes and tweaks I've lost count of them all.
    
    Player class rework complete for now.  Oi vey that took forever and a day.

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+"""
+Author: Geo
+Date: July 29th, 2023
+
+This module contains all global variables used to store game settings, database settings, and frequently
+used constants & methods for easy standardization.  This class is intended to be entirely static for the
+time being.
+
+In a future update, I will likely look into implementing the possibility of running multiple games. At
+that point, each game will need its own instance of this class.
+"""
+
 import asyncio
 from datetime import datetime
 import collections
@@ -5,22 +17,29 @@ import collections
 
 class Config:
     # Game Setup
+    bot = None
+
+    game_host = None  # Currently unused, but useful as a key. Might be useful for separating admin/host perms.
+    guild = None  # Currently not used, may use as a key instead. Logically implies one game per guild.
     signups_open = False  # Pregame
     vote_channel = None
     game_channel = None
-    global_day_length = 1
+    global_day_length = 1  # Rename to "phase_length_in_seconds" and rework
     day_end_time = 1
 
     day_end_task_object = None
 
     # Game Status
     day_number = 0
-    signup_list = []  # Pre & Mid
+    # signup_list = []  # Pre & Mid
     vote_count_number = 1
     votes = collections.OrderedDict()
     live_players = []
     vote_since_last_count = 0  # seems to be unused.
     # start_time = datetime.now()  # Unused atm
+
+    signup_list = collections.OrderedDict()
+    player_list = collections.OrderedDict()
 
     abstained_players = []
 
@@ -29,6 +48,16 @@ class Config:
     posts_per_24_hours_threshold = None  # WIP for use in tracking post activity
     posts_per_phase_threshold = None  # WIP for use in tracking post activity
 
+    # Database info
+    dbManager = None
+    username = None
+    password = None
+    db_host = None
+    database = None
+
+    CONFIG_TABLE = "Configurations"
+    PLAYER_TABLE = "Players"
+
     # CONSTANTS:
     NOT_VOTING = "not voting"  # Refers to a state in which a player either hasn't voted or has unvoted.
     LINE_BREAK = "-" * 40 + "\n"
@@ -36,7 +65,10 @@ class Config:
     # Status & Alignment Framework: WIP doesn't currently account for extra Mafias.
     STATUS_ALIVE = "Alive"
     STATUS_DEAD = "Dead"
+    STATUS_INACTIVE = "Inactive"
     STATUS_REPLACED = "Replaced"
+    STATUS_MODKILLED = "Modkilled"  # For use when implementing penalizing modkill functionality.
+
     ALIGNMENT_TOWN = "Town"
     ALIGNMENT_MAFIA = "Mafia"
     ALIGNMENT_3RD_PARTY = "3rd Party"
@@ -46,3 +78,86 @@ class Config:
     DEAD_ROLE = "Dead"
     SPECTATOR_ROLE = "Spectator"
     SPOILER_ROLE = "Spoilers"
+
+    # WIP - Assumes that the argument is a Player.
+    @classmethod
+    def appendPlayer(self, new_player):
+        """Add a player to the game - i.e. signup"""
+        Config.signup_list[new_player.member] = new_player
+        # Config.players_by_member[new_player.member] = new_player
+        # Config.players_by_order[new_player.signup_number] = new_player
+
+    # WIP - Assumes that the argument is a Player.
+    @classmethod
+    def removePlayer(self, player_to_remove):
+        """Remove a player from the game - i.e. unsignup"""
+        Config.players_by_member.pop(player_to_remove.member, None)
+        Config.players_by_order.pop(player_to_remove.member, None)
+    @classmethod
+    def convertDaysToSeconds(self, days):
+        return days * 24 * 60 * 60
+    @classmethod
+    def convertSecondsToDays(self, seconds):
+        return ((seconds / 24) / 60) / 60
+
+    @classmethod
+    def organize_signup_list(cls, player=None, position=None):
+        """Use this function to organize the list based on the entries in the database"""
+
+    # WIP - Consider how resetting Config affects the database info.
+    @classmethod
+    def configReset(cls):
+        """This function resets all the static values of the Config class back to their defaults"""
+        Config.game_host = None  # Currently unused, but useful as a key. Might be useful for separating admin/host perms.
+        Config.guild = None  # Currently not used, may use as a key instead. Logically implies one game per guild.
+        Config.signups_open = False  # Pregame
+        Config.vote_channel = None
+        Config.game_channel = None
+        Config.global_day_length = 1  # Rename to "phase_length_in_seconds" and rework
+        Config.day_end_time = 1
+
+        Config.day_end_task_object = None
+
+        # Game Status
+        Config.day_number = 0
+        # signup_list = []  # Pre & Mid
+        Config.vote_count_number = 1
+        Config.votes.clear()
+        Config.live_players = []  # Obsolete now that Player.status is a thing?
+        Config.vote_since_last_count = 0  # seems to be unused.
+        # start_time = datetime.now()  # Unused atm
+
+        Config.signup_list = collections.OrderedDict()
+        Config.player_list = collections.OrderedDict()
+
+        Config.abstained_players = []
+
+        # Post count related stuff
+        Config.post_counts = collections.defaultdict(lambda: collections.defaultdict(int))
+        Config.posts_per_24_hours_threshold = None  # WIP for use in tracking post activity
+        Config.posts_per_phase_threshold = None  # WIP for use in tracking post activity
+
+    @classmethod
+    def votesReset(cls):
+        Config.votes.clear()
+
+    @classmethod
+    def newDay(cls):
+        Config.day_number += 1
+        Config.vote_count_number = 1
+        Config.votesReset()
+
+    @classmethod
+    def configReport(cls):
+        print(f"This is Config.game_host: {Config.game_host.name}" if Config.game_host else f"This is Config.game_host: {Config.game_host}")
+        print(f"This is Config.guild: {Config.guild.name}" if Config.guild else f"This is Config.guild: {Config.guild}")
+        print(f"This is Config.signups_open: {Config.signups_open}")
+        print(f"This is Config.vote_channel: {Config.vote_channel.name}" if Config.vote_channel else f"This is Config.vote_channel: {Config.vote_channel}")
+        print(f"This is Config.game_channel: {Config.game_channel.name}" if Config.game_channel else f"This is Config.game_channel: {Config.game_channel}")
+
+        print(f"This is Config.global_day_length: {Config.global_day_length}")
+        print(f"This is Config.day_end_time: {Config.day_end_time}")
+        print(f"This is Config.day_number: {Config.day_number}")
+        print(f"This is Config.vote_count_number: {Config.vote_count_number}")
+        print(f"This is Config.votes: {key} VOTE: '{value}'" for key, value in Config.votes.items())
+        return
--- player.py
+++ player.py
@@ -1,51 +1,119 @@
+"""
+Author: Geo
+Date: July 29th, 2023
+
+This module is used to host the Player class, which stores Player information. Currently, it only tracks
+a limited number of features, including vote, status, and member.  In the future, it should also track
+role info, alignment, and post count to assist the corresponding future modules we hope to add.
+"""
+
+
 import discord
 from config import Config
+import asyncio
 
 class Player:
-    def __init__(self, member: discord.Member, status, signup_number):
+    def __init__(self, member: discord.Member, status, signup_number=None, vote=Config.NOT_VOTING):
         self.member = member  # Store Discord Info
-        self.id = member.id  # Uniquely identify the player.
-        self.status = status  # Is the player Alive or Dead?
-        self.vote = None  # This might not be worth using - it's more convenient having a dictionary for the VC.
-        self.signup_number = signup_number # For use when kicking a player who has left the server.
+        self.status = status  # Is the player Alive/Dead/Inactive/Replaced/Modkilled?  Inactive = signups before game.
+        # self.vote = vote  # Storing vote seems unnecessary at present time.
+        self.signup_number = signup_number  # For use when kicking a player who has left the server.
 
         # WIP - Toggleable configurations
         self.give_spoilers_on_death = False  # WIP - Allow as an optional configuration in the future.
 
         # Currently unused - for use in helping mods check for prods & for post limiting
-        self.total_posts = 0  # Track the total posts of the user over the course of the game - low use
-        self.recent_posts = 0  # Track the posts of the user within a set time period - typically 24 IRL hours.
-        self.phase_posts = 0  # Track the posts of the user within the Day Phase.
+        self.total_posts = 0  # Track the total posts of the player_to_remove over the course of the game - low use
+        self.recent_posts = 0  # Track the posts of the player_to_remove within a set time period - typically 24 IRL hours.
+        self.phase_posts = 0  # Track the posts of the player_to_remove within the Day Phase.
 
         # Currently unused - for use when automating flips.
         self._role = None
         self._alignment = None
 
-    def activate(self):
+    # WIP - Non-functional
+    def unvote(self):
+        """Better place to store unvote function?"""
+
+    # WIP - function to kill the player. To replace bot's kill(). Admin modkill() should be a wrapper to call this.
+    async def kill(self):
+        """This function is intended to remove a player from the game while the game is active"""
+        print(f"player.kill has been called on {self.member.display_name}")
+        print(f"Member {self.member.display_name} has status: {self.status}")
+        if self.member in Config.signup_list and self.status == Config.STATUS_ALIVE:
+            Config.signup_list.pop(self.member)
+
+            # Set Roles
+            alive_role = discord.utils.get(Config.game_channel.guild.roles, name="Alive")
+            dead_role = discord.utils.get(Config.game_channel.guild.roles, name="Dead")
+            if dead_role is None:
+                dead_role = await Config.game_channel.guild.create_role(name="Dead")
+            if alive_role in self.member.roles:
+                await self.member.remove_roles(alive_role)
+
+            # Unvote:
+            prev_vote = None
+            current_vote = None
+            voter = None
+            if self.member in Config.votes:
+                prev_vote = Config.votes.pop(self.member)
+                current_vote = Config.NOT_VOTING
+                voter = self.member
+                await Config.game_channel.send(f"{self.member.display_name} has unvoted {prev_vote.display_name}.")
+
+            # Finish the kill
+            self.status = Config.STATUS_DEAD
+            if dead_role not in self.member.roles:
+                print(f"!!!KILL SANITY CHECK!!!: {self.member.name} should receive DEAD role")
+                # await member.add_roles(dead_role)  # To be added back later
+            await Config.game_channel.send(
+                f"{self.member.display_name} has been removed from the game. "
+                f"NOTE: Dead role must be added manually for now")
+
+            return prev_vote, current_vote, voter
+
+    def display(self):
+        """This function displays members attributes"""
+
+        print(f"this is self.member.display_name: {self.member.display_name}")
+        print(f"this is self.status: {self.member.status}")
+        print(f"this is self.signup_number: {self.singup_number}")
+        print(f"this is self.vote.display_name: {self.vote.display_name}")
+
+    def displayPlayerName(self):
+        """This function returns the player member's display_name"""
+        return self.member.display_name
+
+    # WIP - Used when adding a player to the game. Status/Role = Alive & add to Config.player_list
+    async def activate(self):
         """The player is placed into the game."""
-        self.vote = None  # remove vote if it exists.
-        self.status = Config.STATUS_ALIVE  # Set status to Alive
-        self.setRole(self._status)
+        self.status = Config.STATUS_ALIVE
 
-    def kill(self):
-        """The player receives the Dead role and can no longer interact with the game"""
-        self.vote = None  # remove vote
-        self.status = Config.STATUS_DEAD  # Set status to Dead
-        # if self.give_spoilers = True, give the Spoilers role on Death.  # To be added later
+        alive_role = discord.utils.get(Config.game_channel.guild.roles, name="Alive")
+        if alive_role is None:
+            alive_role = await Config.game_channel.guild.create_role(name="Alive")
+        await self.member.add_roles(alive_role)
 
+        Config.player_list[self.member] = Config.signup_list[self.member]
+
+    "==============================================================================================="
+    "==============================================================================================="
+    "==============================================================================================="
+
+    # WIP - Non-functional
     def modkill(self):
         """A harsher version of a regular kill."""
         """Player becomes Survivor and is considered to have lost the game."""
-        self._vote = None  # remove vote
-        self._status = Config.STATUS_DEAD  # Set status to Dead
+        self.vote = None  # remove vote
+        self.status = Config.STATUS_DEAD  # Set status to Dead
         self._role = "Survivor"
         self._alignment = Config.ALIGNMENT_3RD_PARTY
 
-    # WIP
+    # WIP - Non-functional
     def setRole(self, roleName):
         """Set a player's role & alingnment  Might expand to setting Spec/Spoiler privileges"""
 
-    # WIP
+    # WIP - Non-functional
     def rateLimitPosts(self, target):
 
         """Set member's server role to one affected by slow mode"""
@@ -53,24 +121,24 @@ class Player:
         """Discord is stupid and won't give perms to bypass slow mode directly"""
         """You either have manage message permissions or nothing at all."""
 
-    # WIP
+    # WIP - Non-functional
     def sendRolePm(self):
-        """Send the user's role PM to them privately.  Should be accessible with a command"""
+        """Send the player_to_remove's role PM to them privately.  Should be accessible with a command"""
 
-    # WIP
+    # WIP - Non-functional
     def confirmRolePM(self, role, alignment):
-        """Require the user to confirm their role and alignment to the bot"""
+        """Require the player_to_remove to confirm their role and alignment to the bot"""
 
-    # WIP
+    # WIP - Non-functional
     def roleReveal(self):
-        """When the user dies, reveal their role.  May also be accessed directly by Innocent Child?"""
+        """When the player_to_remove dies, reveal their role.  May also be accessed directly by Innocent Child?"""
         # I'm concerned that allowing an Innocent Child direct access might lead to removing niche gambits.
 
-    # WIP
+    # WIP - Non-functional
     def flipRole(self):
-        """Reveal the player's role and alignment.  Should be called by a %flip @user command"""
+        """Reveal the player's role and alignment.  Should be called by a %flip @player_to_remove command"""
 
-    # WIP
+    # WIP - Non-functional
     def giveThread(self):
         """Allow a member to create a private thread for their thoughts"""
         """Disinclined to implement this, but leaving it in the structure for now"""
