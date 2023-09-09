1f2b03eb76de08f14affbe675553fdfb9b5f58a8
VysePresident <imgenius77x7@gmail.com>
2023-09-09 06:01:38 -0500

    DB Conn restored on reload
    The database connection will be restored after reloading.

--- main.py
+++ main.py
@@ -11,39 +11,31 @@ from db import DatabaseManager
 from config import Config
 
 bot_token = ''
-conn_info = {}
 try:
     from aso_key_file_name import mysterious_key_of_power, username, password, host, database
-    print("Main key imported")
     bot_token = mysterious_key_of_power
-    Config.username = username
-    Config.password = password
-    Config.db_host = host
-    Config.database = database
+    print("Main key imported")
 except ImportError:
     try:
         from test_token import test_token, username, password, host, database
         bot_token = test_token
         print("Test bot key imported")
-        Config.username = username
-        Config.password = password
-        Config.db_host = host
-        Config.database = database
     except ImportError:
-        bot_token = ''
+        bot_token = test_token = mysterious_key_of_power = None
         print("All imports failed.  Using blank key")
 
 bot = MafiaBot(command_prefix="%", token=bot_token)
-Config.dbManager = DatabaseManager(Config.username, Config.password, Config.db_host, Config.database, bot)
-print(f'This is username: {Config.username} this is password: {Config.password}')
-print(f'This is host: {Config.db_host} this is database: {Config.database}')
-
 
 @bot.event
 async def on_ready():
 
     await bot.load_extension("admin_commands")
     await bot.load_extension("player_commands")
+
+    await db_conn()
+
+    Config.dbManager = DatabaseManager(Config.username, Config.password, Config.db_host, Config.database, bot)
+    print(f'This is database: {Config.database}')
 
     await Config.dbManager.connect()
     connResult = await Config.dbManager.setConfigurations()
@@ -64,5 +56,27 @@ async def reloadext(ctx):
     print("Reloading extensions!")
     await bot.reload_extension("admin_commands")
     await bot.reload_extension("player_commands")
+
+
+async def db_conn():
+    try:
+        from aso_key_file_name import username, password, host, database
+        print("Main db info imported")
+        Config.username = username
+        Config.password = password
+        Config.db_host = host
+        Config.database = database
+    except ImportError:
+        try:
+            from test_token import username, password, host, database
+            print("Test db info imported")
+            Config.username = username
+            Config.password = password
+            Config.db_host = host
+            Config.database = database
+        except ImportError:
+            username = password = host = database = None
+            print("All imports failed. Not connected to database")
+    
 
 bot.run(bot_token)
